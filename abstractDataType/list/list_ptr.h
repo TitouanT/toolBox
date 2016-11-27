4233ad3289929cbf4d444d6280478bcdb6f15ff1
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-27 21:36:18 +0100

    Update list_ptr.h

--- abstractDataType/list/list_ptr.h
+++ abstractDataType/list/list_ptr.h
@@ -21,20 +21,20 @@ typedef enum {some, name, in, your, enum} t_data;
 typedef <existingtype> t_data;
 */
 
-void listPtr_appendEnd (t_data data);	//ok
-void listPtr_appendHead (t_data data);	//ok
-void listPtr_appendLeft (t_data data);	//ok
-void listPtr_appendRight (t_data data);	//ok
-void listPtr_init (void);				//ok
-int  listPtr_isEmpty (void);			//ok
+void listPtr_appendEnd (t_data data);
+void listPtr_appendHead (t_data data);
+void listPtr_appendLeft (t_data data);
+void listPtr_appendRight (t_data data);
+void listPtr_init (void);
+int  listPtr_isEmpty (void);
 int  listPtr_isInList (t_data data);    // !ok, you have to code it !
-int  listPtr_isOut (void);				//ok
-void listPtr_move2end (void);			//ok
-void listPtr_move2head (void);			//ok
-void listPtr_next (void);				//ok
-void listPtr_prev (void);				//ok
-void listPtr_readData (t_data * data);	//ok
-void listPtr_removeElt (void); 			//ok
-void listPtr_removeList (void); 		//ok
+int  listPtr_isOut (void);
+void listPtr_move2end (void);
+void listPtr_move2head (void);
+void listPtr_next (void);
+void listPtr_prev (void);
+void listPtr_readData (t_data * data);
+void listPtr_removeElt (void);
+void listPtr_removeList (void);
 
 #endif /* end of include guard: LIST_PTR_H */
